1a9f5da37873b45af9c1dc641cff3be5c51aa62f
Antonio VA Hilario <avahilario@gmail.com>
2020-06-10 18:11:51 +0000

    PDF files fetched via wget --mirror on or before 20200610-181151

--- pack-path.sh
+++ pack-path.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-PACKSIZE=20
+PACKLIMIT=10
+SIZELIMIT=10000
 diff --context=$(cat lisdata.current | wc -l) lisdata.{exclude,current} | grep '^+' | sed -r -e 's@^\+ @@g' > lisdata.keep
 Q=0
 while true; do
   N=$(cat lisdata.keep | wc -l)
   M=$N
-  P=$PACKSIZE
-  [ $N -lt $PACKSIZE ] && P=$N 
+  PACKSIZE=1
+
+  # Keep adding files to the pack until $SIZE exceeds threshold
+  while [ $PACKSIZE -lt $PACKLIMIT ]; do
+    P=$PACKSIZE
+    [ $N -lt $PACKSIZE ] && P=$N 
+    head -n$P lisdata.keep > lisdata.pack.tmp
+    SIZE=$(du -c $(cat lisdata.pack.tmp) | tail -n1 | sed -r -e 's@^([0-9]{1,}).*@\1@g')
+    if [ $SIZE -gt $SIZELIMIT ]; then
+      break
+    fi
+    ((PACKSIZE++))
+  done
+
   cat <<EOF > lisdata.pack
 #!/bin/bash
 DATESTAMP=\$(date +"%Y%m%d-%H%M%S")
 EOF
-  head -n$P lisdata.keep >> lisdata.pack
- 
+  cat lisdata.pack.tmp >> lisdata.pack
+
   ((N-=P))
   tail -n$N lisdata.keep > lisdata.remainder
   mv lisdata.remainder lisdata.keep
-  echo $M $N
+  #echo $M $N
   sed -i -r -e 's@^([^#$]{1,})$@  git add "\1"@g' lisdata.pack
   cat <<EOF >> lisdata.pack
 git commit -a -m "PDF files fetched via wget --mirror on or before \$DATESTAMP"
@@ -25,6 +38,7 @@ EOF
 
   chmod 755 lisdata.pack
   cat lisdata.pack
+  echo "Size: ${SIZE}"
   bash lisdata.pack
   sleep 3
 
